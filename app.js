00a5bb2f4b802edec9705b35175dc13f94693429
Andrés Osante <aosantea@ucenfote.ac.cr>
2018-12-24 12:51:54 -0600

    deployment changes

--- app.js
+++ app.js
@@ -3,40 +3,46 @@ const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://admin:admin123@ds059938.mlab.com:59938/nosotros_tequila', {useNewUrlParser: true});
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  'mongodb://admin:admin123@ds059938.mlab.com:59938/nosotros_tequila';
+
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true }
+);
 let db = mongoose.connection;
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Inicializando el app
 const app = express();
 
 //middleware de bodyparser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Server start
 app.listen(port, () => {
-    console.log('Server started on port ' + port);
-})
+  console.log('Server started on port ' + port);
+});
 
 //chequear si hay conexion exitosa
 db.once('open', () => {
-    console.log('Connected to mongodb');
-})
+  console.log('Connected to mongodb');
+});
 
 //chequear si hay errores en la conexion
 db.on('error', err => {
-    console.log(err)
-}) 
+  console.log(err);
+});
 
-let User = require('./models/user.model');``
+let User = require('./models/user.model');
+``;
 
 //Cargar los archivos html para poder utilizarlos
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
 
 //Seteando el public folder para que estén disponibles los archivos de css, js etc..
 app.use(express.static(path.join(__dirname, 'public')));
@@ -45,8 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const users = require('./routes/user.route');
 app.use('/users', users);
 
-
 //Ruta del home page
 app.get('/', (req, res) => {
-    res.render('index.html');
-})
+  res.render('index.html');
+});
